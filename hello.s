5e37d362c566d78b3ded3e26a73026d194daa92e
Ralf Th. Pietsch <ratopi@abwesend.de>
2023-08-22 22:44:08 +0200

    Hello World! :-)

--- hello.s
+++ hello.s
@@ -1,9 +1,19 @@
 .global _start
 
 _start:
+	# stdout = (fd) 1
+	addi a7, zero, 64
+	addi a0, zero, 1
+	la a1, helloworld
+	addi a2, zero, 15
+	ecall
 
 	# Exit with code 13
 	addi a7, zero, 93
 	addi a0, zero, 13
 	ecall
+
+
+helloworld:
+	.ascii "Hello, World!\n"
 
